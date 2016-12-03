8b4bde21b23b965f344cafcbd088e3c3b0150252
w <w@composite-dev-vm2.localdomain>
2016-12-03 17:26:43 -0500

    initial commit using SQLite amalgamation 3.15.2

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-DSQLITE_OS_OTHER=1
+SRC=$(wildcard *.c)
+HDR=$(wildcard *.h)
+EXE=sqlite
+
+all:
+	$(CC) $(CFLAGS) -o $(EXE) $(SRC)
---
+++ os_composite.c
@@ -0,0 +1,112 @@
+/*
+** 2004 May 22
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+******************************************************************************
+**
+** This file contains the VFS implementation for Composite.
+*/
+#include "sqlite3.h"
+#if SQLITE_OS_OTHER
+
+/** sqlite3_vfs methods */
+static int cOpen(sqlite3_vfs*, const char *zName, sqlite3_file*, int flags, int *pOutFlags) {}
+static int cDelete(sqlite3_vfs*, const char *zName, int syncDir) {}
+static int cAccess(sqlite3_vfs*, const char *zName, int flags, int *pResOut) {}
+static int cFullPathname(sqlite3_vfs*, const char *zName, int nOut, char *zOut) {}
+
+static int cRandomness(sqlite3_vfs*, int nByte, char *zOut) {}
+static int cSleep(sqlite3_vfs*, int microseconds) {}
+static int cCurrentTime(sqlite3_vfs*, double*) {}
+static int cGetLastError(sqlite3_vfs*, int, char *) {}
+static int cCurrentTimeInt64(sqlite3_vfs*, sqlite3_int64*) {}
+
+/* sqlite3_io_methods */
+struct cFile {
+    struct sqlite3_io_methods* composite_io_methods;
+};
+
+static int cClose(sqlite3_file*) {}
+static int cRead(sqlite3_file*, void*, int iAmt, sqlite3_int64 iOfst) {}
+static int cWrite(sqlite3_file*, const void*, int iAmt, sqlite3_int64 iOfst) {}
+static int cTruncate(sqlite3_file*, sqlite3_int64 size) {}
+static int cSync(sqlite3_file*, int flags) {}
+static int cFileSize(sqlite3_file*, sqlite3_int64 *pSize) {}
+static int cLock(sqlite3_file*, int) {}
+static int cUnlock(sqlite3_file*, int) {}
+static int cCheckReservedLock(sqlite3_file*, int *pResOut) {}
+static int cFileControl(sqlite3_file*, int op, void *pArg) {}
+static int cSectorSize(sqlite3_file*) {}
+static int cDeviceCharacteristics(sqlite3_file*) {}
+static int cShmMap(sqlite3_file*, int iPg, int pgsz, int, void volatile**) {}
+static int cShmLock(sqlite3_file*, int offset, int n, int flags) {}
+static void cShmBarrier(sqlite3_file*) {}
+static int cShmUnmap(sqlite3_file*, int deleteFlag) {}
+static int cFetch(sqlite3_file*, sqlite3_int64 iOfst, int iAmt, void **pp) {}
+static int cUnfetch(sqlite3_file*, sqlite3_int64 iOfst, void *p) {}
+
+static const struct sqlite3_io_methods composite_io_methods = {
+    .iVersion = 3,
+    .xClose = cClose,
+    .xRead = cRead,
+    .xWrite = cWrite,
+    .xTruncate = cTruncate,
+    .xSync = cSync,
+    .xFileSize = cFileSize,
+    .xLock = cLock,
+    .xUnlock = cUnlock,
+    .xCheckReservedLock = cCheckReservedLock,
+    .xFileControl = cFileControl,
+    .xSectorSize = cSectorSize,
+    .xDeviceCharacteristics = cDeviceCharacteristics,
+    .xShmMap = cShmMap,
+    .xShmLock = cShmLock,
+    .xShmBarrier = cShmBarrier,
+    .xShmUnmap = cShmUnmap,
+    .xFetch = cFetch,
+    .xUnfetch = cUnfetch
+};
+
+static sqlite3_vfs composite_vfs = {
+    .iVersion = 3,
+    .szOsFile = sizeof(struct cFile),
+    .mxPathname = /* TODO */,
+    .pNext = 0,
+    .zName = "composite",
+    .pAppData = /* TODO */,
+    .xOpen = cOpen,
+    .xDelete = cDelete,
+    .xAccess = cAccess,
+    .xFullPathname = cFullPathname,
+    .xDlOpen = 0,
+    .xDlError = 0,
+    .xDlSym = 0,
+    .xDlClose = 0,
+    .xRandomness = cRandomness,
+    .xSleep = cSleep,
+    .xCurrentTime = cCurrentTime,
+    .xGetLastError = cGetLastError,
+    .xCurrentTimeInt64 = cCurrentTimeInt64,
+    .xSetSystemCall = 0,
+    .xGetSystemCall = 0,
+    .xNextSystemCall = 0
+};
+
+/* init the OS interface */
+int sqlite3_os_init(void){ 
+  sqlite3_vfs_register(&composite_vfs, 1);
+  return SQLITE_OK;
+}
+
+/* shutdown the OS interface */
+int sqlite3_os_end(void) { 
+  return SQLITE_OK; 
+}
+
+#endif
