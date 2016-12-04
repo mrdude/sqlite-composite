73bb84777c5f139264589c8bca46f09f66735d1f
Warren S <mr.dude1848@gmail.com>
2016-12-04 14:36:39 -0500

    replaced nested macro

--- os_composite.h
+++ os_composite.h
@@ -24,98 +24,98 @@
     } while(0)
 
 #define CTRACE_APPEND(...) do { snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch), __VA_ARGS__ ); } while(0)
-#define CTRACE_PRINT(...) do { printf("%s", &_ch[0]); } while(0)
+#define CTRACE_PRINT(...) do { printf("%s\n", &_ch[0]); } while(0)
 
 #define APPEND_ERR_CODE(x) do { \
     switch( x ) { \
-        case SQLITE_OK: CTRACE_APPEND("SQLITE_OK"); break; \
-        case SQLITE_ERROR: CTRACE_APPEND("SQLITE_ERROR"); break; \
-        case SQLITE_INTERNAL: CTRACE_APPEND("SQLITE_INTERNAL"); break; \
-        case SQLITE_PERM: CTRACE_APPEND("SQLITE_PERM"); break; \
-        case SQLITE_ABORT: CTRACE_APPEND("SQLITE_ABORT"); break; \
-        case SQLITE_BUSY: CTRACE_APPEND("SQLITE_BUSY"); break; \
-        case SQLITE_LOCKED: CTRACE_APPEND("SQLITE_LOCKED"); break; \
-        case SQLITE_NOMEM: CTRACE_APPEND("SQLITE_NOMEM"); break; \
-        case SQLITE_READONLY: CTRACE_APPEND("SQLITE_READONLY"); break; \
-        case SQLITE_INTERRUPT: CTRACE_APPEND("SQLITE_INTERRUPT"); break; \
-        case SQLITE_IOERR: CTRACE_APPEND("SQLITE_IOERR"); break; \
-        case SQLITE_CORRUPT: CTRACE_APPEND("SQLITE_CORRUPT"); break; \
-        case SQLITE_NOTFOUND: CTRACE_APPEND("SQLITE_NOTFOUND"); break; \
-        case SQLITE_FULL: CTRACE_APPEND("SQLITE_FULL"); break; \
-        case SQLITE_CANTOPEN: CTRACE_APPEND("SQLITE_CANTOPEN"); break; \
-        case SQLITE_PROTOCOL: CTRACE_APPEND("SQLITE_PROTOCOL"); break; \
-        case SQLITE_EMPTY: CTRACE_APPEND("SQLITE_EMPTY"); break; \
-        case SQLITE_SCHEMA: CTRACE_APPEND("SQLITE_SCHEMA"); break; \
-        case SQLITE_TOOBIG: CTRACE_APPEND("SQLITE_TOOBIG"); break; \
-        case SQLITE_CONSTRAINT: CTRACE_APPEND("SQLITE_CONSTRAINT"); break; \
-        case SQLITE_MISMATCH: CTRACE_APPEND("SQLITE_MISMATCH"); break; \
-        case SQLITE_MISUSE: CTRACE_APPEND("SQLITE_MISUSE"); break; \
-        case SQLITE_NOLFS: CTRACE_APPEND("SQLITE_NOLFS"); break; \
-        case SQLITE_AUTH: CTRACE_APPEND("SQLITE_AUTH"); break; \
-        case SQLITE_FORMAT: CTRACE_APPEND("SQLITE_FORMAT"); break; \
-        case SQLITE_RANGE: CTRACE_APPEND("SQLITE_RANGE"); break; \
-        case SQLITE_NOTADB: CTRACE_APPEND("SQLITE_NOTADB"); break; \
-        case SQLITE_NOTICE: CTRACE_APPEND("SQLITE_NOTICE"); break; \
-        case SQLITE_WARNING: CTRACE_APPEND("SQLITE_WARNING"); break; \
-        case SQLITE_ROW: CTRACE_APPEND("SQLITE_ROW"); break; \
-        case SQLITE_DONE: CTRACE_APPEND("SQLITE_DONE"); break; \
-        case SQLITE_IOERR_READ: CTRACE_APPEND("SQLITE_"); break; \
-        case SQLITE_IOERR_SHORT_READ: CTRACE_APPEND("SQLITE_IOERR_SHORT_READ"); break; \
-        case SQLITE_IOERR_WRITE: CTRACE_APPEND("SQLITE_IOERR_WRITE"); break; \
-        case SQLITE_IOERR_FSYNC: CTRACE_APPEND("SQLITE_IOERR_FSYNC"); break; \
-        case SQLITE_IOERR_DIR_FSYNC: CTRACE_APPEND("SQLITE_IOERR_DIR_FSYNC"); break; \
-        case SQLITE_IOERR_TRUNCATE: CTRACE_APPEND("SQLITE_IOERR_TRUNCATE"); break; \
-        case SQLITE_IOERR_FSTAT: CTRACE_APPEND("SQLITE_IOERR_FSTAT"); break; \
-        case SQLITE_IOERR_UNLOCK: CTRACE_APPEND("SQLITE_IOERR_UNLOCK"); break; \
-        case SQLITE_IOERR_RDLOCK: CTRACE_APPEND("SQLITE_IOERR_RDLOCK"); break; \
-        case SQLITE_IOERR_DELETE: CTRACE_APPEND("SQLITE_IOERR_DELETE"); break; \
-        case SQLITE_IOERR_BLOCKED: CTRACE_APPEND("SQLITE_IOERR_BLOCKED"); break; \
-        case SQLITE_IOERR_NOMEM: CTRACE_APPEND("SQLITE_IOERR_NOMEM"); break; \
-        case SQLITE_IOERR_ACCESS: CTRACE_APPEND("SQLITE_IOERR_ACCESS"); break; \
-        case SQLITE_IOERR_CHECKRESERVEDLOCK: CTRACE_APPEND("SQLITE_IOERR_CHECKRESERVEDLOCK"); break; \
-        case SQLITE_IOERR_LOCK: CTRACE_APPEND("SQLITE_IOERR_LOCK"); break; \
-        case SQLITE_IOERR_CLOSE: CTRACE_APPEND("SQLITE_IOERR_CLOSE"); break; \
-        case SQLITE_IOERR_DIR_CLOSE: CTRACE_APPEND("SQLITE_IOERR_DIR_CLOSE"); break; \
-        case SQLITE_IOERR_SHMOPEN: CTRACE_APPEND("SQLITE_IOERR_SHMOPEN"); break; \
-        case SQLITE_IOERR_SHMSIZE: CTRACE_APPEND("SQLITE_IOERR_SHMSIZE"); break; \
-        case SQLITE_IOERR_SHMLOCK: CTRACE_APPEND("SQLITE_IOERR_SHMLOCK"); break; \
-        case SQLITE_IOERR_SHMMAP: CTRACE_APPEND("SQLITE_IOERR_SHMMAP"); break; \
-        case SQLITE_IOERR_SEEK: CTRACE_APPEND("SQLITE_IOERR_SEEK"); break; \
-        case SQLITE_IOERR_DELETE_NOENT: CTRACE_APPEND("SQLITE_IOERR_DELETE_NOENT"); break; \
-        case SQLITE_IOERR_MMAP: CTRACE_APPEND("SQLITE_IOERR_MMAP"); break; \
-        case SQLITE_IOERR_GETTEMPPATH: CTRACE_APPEND("SQLITE_IOERR_GETTEMPPATH"); break; \
-        case SQLITE_IOERR_CONVPATH: CTRACE_APPEND("SQLITE_IOERR_CONVPATH"); break; \
-        case SQLITE_IOERR_VNODE: CTRACE_APPEND("SQLITE_IOERR_VNODE"); break; \
-        case SQLITE_IOERR_AUTH: CTRACE_APPEND("SQLITE_IOERR_AUTH"); break; \
-        case SQLITE_LOCKED_SHAREDCACHE: CTRACE_APPEND("SQLITE_LOCKED_SHAREDCACHE"); break; \
-        case SQLITE_BUSY_RECOVERY: CTRACE_APPEND("SQLITE_BUSY_RECOVERY"); break; \
-        case SQLITE_BUSY_SNAPSHOT: CTRACE_APPEND("SQLITE_BUSY_SNAPSHOT"); break; \
-        case SQLITE_CANTOPEN_NOTEMPDIR: CTRACE_APPEND("SQLITE_CANTOPEN_NOTEMPDIR"); break; \
-        case SQLITE_CANTOPEN_ISDIR: CTRACE_APPEND("SQLITE_CANTOPEN_ISDIR"); break; \
-        case SQLITE_CANTOPEN_FULLPATH: CTRACE_APPEND("SQLITE_CANTOPEN_FULLPATH"); break; \
-        case SQLITE_CANTOPEN_CONVPATH: CTRACE_APPEND("SQLITE_CANTOPEN_CONVPATH"); break; \
-        case SQLITE_CORRUPT_VTAB: CTRACE_APPEND("SQLITE_CORRUPT_VTAB"); break; \
-        case SQLITE_READONLY_RECOVERY: CTRACE_APPEND("SQLITE_READONLY_RECOVERY"); break; \
-        case SQLITE_READONLY_CANTLOCK: CTRACE_APPEND("SQLITE_READONLY_CANTLOCK"); break; \
-        case SQLITE_READONLY_ROLLBACK: CTRACE_APPEND("SQLITE_READONLY_ROLLBACK"); break; \
-        case SQLITE_READONLY_DBMOVED: CTRACE_APPEND("SQLITE_READONLY_DBMOVED"); break; \
-        case SQLITE_ABORT_ROLLBACK: CTRACE_APPEND("SQLITE_ABORT_ROLLBACK"); break; \
-        case SQLITE_CONSTRAINT_CHECK: CTRACE_APPEND("SQLITE_CONSTRAINT_CHECK"); break; \
-        case SQLITE_CONSTRAINT_COMMITHOOK: CTRACE_APPEND("SQLITE_CONSTRAINT_COMMITHOOK"); break; \
-        case SQLITE_CONSTRAINT_FOREIGNKEY: CTRACE_APPEND("SQLITE_CONSTRAINT_FOREIGNKEY"); break; \
-        case SQLITE_CONSTRAINT_FUNCTION: CTRACE_APPEND("SQLITE_CONSTRAINT_FUNCTION"); break; \
-        case SQLITE_CONSTRAINT_NOTNULL: CTRACE_APPEND("SQLITE_CONSTRAINT_NOTNULL"); break; \
-        case SQLITE_CONSTRAINT_PRIMARYKEY: CTRACE_APPEND("SQLITE_CONSTRAINT_PRIMARYKEY"); break; \
-        case SQLITE_CONSTRAINT_TRIGGER: CTRACE_APPEND("SQLITE_CONSTRAINT_TRIGGER"); break; \
-        case SQLITE_CONSTRAINT_UNIQUE: CTRACE_APPEND("SQLITE_CONSTRAINT_UNIQUE"); break; \
-        case SQLITE_CONSTRAINT_VTAB: CTRACE_APPEND("SQLITE_CONSTRAINT_VTAB"); break; \
-        case SQLITE_CONSTRAINT_ROWID: CTRACE_APPEND("SQLITE_CONSTRAINT_ROWID"); break; \
-        case SQLITE_NOTICE_RECOVER_WAL: CTRACE_APPEND("SQLITE_NOTICE_RECOVER_WAL"); break; \
-        case SQLITE_NOTICE_RECOVER_ROLLBACK: CTRACE_APPEND("SQLITE_NOTICE_RECOVER_ROLLBACK"); break; \
-        case SQLITE_WARNING_AUTOINDEX: CTRACE_APPEND("SQLITE_WARNING_AUTOINDEX"); break; \
-        case SQLITE_AUTH_USER: CTRACE_APPEND("SQLITE_AUTH_USER"); break; \
-        case SQLITE_OK_LOAD_PERMANENTLY: CTRACE_APPEND("SQLITE_OK_LOAD_PERMANENTLY"); break; \
-        default: CTRACE_APPEND("Unknown(%d)", x ); break; \
+        case SQLITE_OK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_OK"); break; \
+        case SQLITE_ERROR: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_ERROR"); break; \
+        case SQLITE_INTERNAL: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_INTERNAL"); break; \
+        case SQLITE_PERM: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_PERM"); break; \
+        case SQLITE_ABORT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_ABORT"); break; \
+        case SQLITE_BUSY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_BUSY"); break; \
+        case SQLITE_LOCKED: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_LOCKED"); break; \
+        case SQLITE_NOMEM: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOMEM"); break; \
+        case SQLITE_READONLY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_READONLY"); break; \
+        case SQLITE_INTERRUPT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_INTERRUPT"); break; \
+        case SQLITE_IOERR: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR"); break; \
+        case SQLITE_CORRUPT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CORRUPT"); break; \
+        case SQLITE_NOTFOUND: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOTFOUND"); break; \
+        case SQLITE_FULL: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_FULL"); break; \
+        case SQLITE_CANTOPEN: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CANTOPEN"); break; \
+        case SQLITE_PROTOCOL: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_PROTOCOL"); break; \
+        case SQLITE_EMPTY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_EMPTY"); break; \
+        case SQLITE_SCHEMA: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_SCHEMA"); break; \
+        case SQLITE_TOOBIG: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_TOOBIG"); break; \
+        case SQLITE_CONSTRAINT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT"); break; \
+        case SQLITE_MISMATCH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_MISMATCH"); break; \
+        case SQLITE_MISUSE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_MISUSE"); break; \
+        case SQLITE_NOLFS: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOLFS"); break; \
+        case SQLITE_AUTH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_AUTH"); break; \
+        case SQLITE_FORMAT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_FORMAT"); break; \
+        case SQLITE_RANGE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_RANGE"); break; \
+        case SQLITE_NOTADB: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOTADB"); break; \
+        case SQLITE_NOTICE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOTICE"); break; \
+        case SQLITE_WARNING: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_WARNING"); break; \
+        case SQLITE_ROW: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_ROW"); break; \
+        case SQLITE_DONE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_DONE"); break; \
+        case SQLITE_IOERR_READ: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_"); break; \
+        case SQLITE_IOERR_SHORT_READ: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SHORT_READ"); break; \
+        case SQLITE_IOERR_WRITE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_WRITE"); break; \
+        case SQLITE_IOERR_FSYNC: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_FSYNC"); break; \
+        case SQLITE_IOERR_DIR_FSYNC: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_DIR_FSYNC"); break; \
+        case SQLITE_IOERR_TRUNCATE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_TRUNCATE"); break; \
+        case SQLITE_IOERR_FSTAT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_FSTAT"); break; \
+        case SQLITE_IOERR_UNLOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_UNLOCK"); break; \
+        case SQLITE_IOERR_RDLOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_RDLOCK"); break; \
+        case SQLITE_IOERR_DELETE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_DELETE"); break; \
+        case SQLITE_IOERR_BLOCKED: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_BLOCKED"); break; \
+        case SQLITE_IOERR_NOMEM: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_NOMEM"); break; \
+        case SQLITE_IOERR_ACCESS: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_ACCESS"); break; \
+        case SQLITE_IOERR_CHECKRESERVEDLOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_CHECKRESERVEDLOCK"); break; \
+        case SQLITE_IOERR_LOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_LOCK"); break; \
+        case SQLITE_IOERR_CLOSE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_CLOSE"); break; \
+        case SQLITE_IOERR_DIR_CLOSE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_DIR_CLOSE"); break; \
+        case SQLITE_IOERR_SHMOPEN: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SHMOPEN"); break; \
+        case SQLITE_IOERR_SHMSIZE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SHMSIZE"); break; \
+        case SQLITE_IOERR_SHMLOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SHMLOCK"); break; \
+        case SQLITE_IOERR_SHMMAP: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SHMMAP"); break; \
+        case SQLITE_IOERR_SEEK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_SEEK"); break; \
+        case SQLITE_IOERR_DELETE_NOENT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_DELETE_NOENT"); break; \
+        case SQLITE_IOERR_MMAP: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_MMAP"); break; \
+        case SQLITE_IOERR_GETTEMPPATH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_GETTEMPPATH"); break; \
+        case SQLITE_IOERR_CONVPATH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_CONVPATH"); break; \
+        case SQLITE_IOERR_VNODE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_VNODE"); break; \
+        case SQLITE_IOERR_AUTH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_IOERR_AUTH"); break; \
+        case SQLITE_LOCKED_SHAREDCACHE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_LOCKED_SHAREDCACHE"); break; \
+        case SQLITE_BUSY_RECOVERY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_BUSY_RECOVERY"); break; \
+        case SQLITE_BUSY_SNAPSHOT: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_BUSY_SNAPSHOT"); break; \
+        case SQLITE_CANTOPEN_NOTEMPDIR: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CANTOPEN_NOTEMPDIR"); break; \
+        case SQLITE_CANTOPEN_ISDIR: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CANTOPEN_ISDIR"); break; \
+        case SQLITE_CANTOPEN_FULLPATH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CANTOPEN_FULLPATH"); break; \
+        case SQLITE_CANTOPEN_CONVPATH: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CANTOPEN_CONVPATH"); break; \
+        case SQLITE_CORRUPT_VTAB: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CORRUPT_VTAB"); break; \
+        case SQLITE_READONLY_RECOVERY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_READONLY_RECOVERY"); break; \
+        case SQLITE_READONLY_CANTLOCK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_READONLY_CANTLOCK"); break; \
+        case SQLITE_READONLY_ROLLBACK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_READONLY_ROLLBACK"); break; \
+        case SQLITE_READONLY_DBMOVED: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_READONLY_DBMOVED"); break; \
+        case SQLITE_ABORT_ROLLBACK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_ABORT_ROLLBACK"); break; \
+        case SQLITE_CONSTRAINT_CHECK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_CHECK"); break; \
+        case SQLITE_CONSTRAINT_COMMITHOOK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_COMMITHOOK"); break; \
+        case SQLITE_CONSTRAINT_FOREIGNKEY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_FOREIGNKEY"); break; \
+        case SQLITE_CONSTRAINT_FUNCTION: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_FUNCTION"); break; \
+        case SQLITE_CONSTRAINT_NOTNULL: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_NOTNULL"); break; \
+        case SQLITE_CONSTRAINT_PRIMARYKEY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_PRIMARYKEY"); break; \
+        case SQLITE_CONSTRAINT_TRIGGER: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_TRIGGER"); break; \
+        case SQLITE_CONSTRAINT_UNIQUE: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_UNIQUE"); break; \
+        case SQLITE_CONSTRAINT_VTAB: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_VTAB"); break; \
+        case SQLITE_CONSTRAINT_ROWID: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_CONSTRAINT_ROWID"); break; \
+        case SQLITE_NOTICE_RECOVER_WAL: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOTICE_RECOVER_WAL"); break; \
+        case SQLITE_NOTICE_RECOVER_ROLLBACK: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_NOTICE_RECOVER_ROLLBACK"); break; \
+        case SQLITE_WARNING_AUTOINDEX: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_WARNING_AUTOINDEX"); break; \
+        case SQLITE_AUTH_USER: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_AUTH_USER"); break; \
+        case SQLITE_OK_LOAD_PERMANENTLY: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"SQLITE_OK_LOAD_PERMANENTLY"); break; \
+        default: snprintf(&_ch[ strlen(_ch) ], LEN - strlen(_ch),"Unknown(%d)", x ); break; \
     } \
     } while(0)
 #endif
