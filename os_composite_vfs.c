abb5a55840a499760b5ab393237107fadf7d8d8d
Warren S <mr.dude1848@gmail.com>
2016-12-05 11:13:11 -0500

    casting changes

--- os_composite_vfs.c
+++ os_composite_vfs.c
@@ -144,7 +144,7 @@ static int fs_read(struct fs_file* file, int64_t offset, int len, void* buf) {
 
     /* copy the bytes into the buffer */
     if( bytes_read > 0 ) {
-        memmove(buf, file->data.buf[offset], (size_t)bytes_read);
+        memmove(buf, (const void*)file->data.buf[offset], (size_t)bytes_read);
     }
 
     return bytes_read;
@@ -167,7 +167,7 @@ static int fs_write(struct fs_file* file, int64_t offset, int len, const void* b
     }
 
     /* perform the write */
-    memmove( (const void*)file->data.buf[offset], buf, (size_t)len );
+    memmove( (void*)file->data.buf[offset], buf, (size_t)len );
 
     /* adjust file->data.len */
     file->data.len = end_offset;
