d9d9457fd1571ed0f93b4bdc7d07a1f00e8da002
Warren S <mr.dude1848@gmail.com>
2016-12-06 15:16:44 -0500

    run a simple SQL statement

--- shell.c
+++ shell.c
@@ -2,7 +2,44 @@
 
 #include <stdio.h>
 
-int main() {
+int run_example_statement(sqlite3 *db) {
+  sqlite3_stmt *stmt;
+  const char* zSql = "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);";
+  if( sqlite3_prepare(db, zSql, -1, &stmt, 0) != SQLITE_OK ) {
+    return SQLITE_ERROR;
+  }
+
+  int done = 0;
+  while( !done ) {
+    switch( sqlite3_step(stmt) ) {
+      case SQLITE_BUSY:
+        break;
+      case SQLITE_ROW:
+        break;
+      case SQLITE_DONE:
+        done = 1;
+        break;
+      case SQLITE_ERROR:
+      case SQLITE_MISUSE:
+        return SQLITE_ERROR;
+    }
+  }
+
+  if( sqlite3_finalize(stmt) != SQLITE_OK ) {
+    return SQLITE_ERROR;
+  }
+
+  return SQLITE_OK;
+}
+
+int main(void) {
+  sqlite3* db;
+  sqlite3_open("dh", &db);
   printf("Hello whirled!\n");
+
+  printf("%s\n", run_example_statement(db) == SQLITE_OK ? "ok" : "error");
+
+  printf("Goodbye!\n");
+  sqlite3_close(db);
   return 0;
 }
