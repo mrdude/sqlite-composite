feec266264df25dfc42108bad12f727450e831f6
Warren S <mr.dude1848@gmail.com>
2016-12-06 15:56:29 -0500

    print resultset using sqlite3_get_table()

--- shell.c
+++ shell.c
@@ -2,73 +2,33 @@
 
 #include <stdio.h>
 
-void print_statement_columns(sqlite3_stmt *stmt) {
-  const int col_count = sqlite3_column_count(stmt);
-  int i;
-
-  for( i = 0; i < col_count; i++ ) {
-    /* print column ID */
-    printf("Column %d: ", i);
-
-    /* print column type */
-    switch( sqlite3_column_type(stmt, i) ) {
-      case SQLITE_INTEGER: printf("Integer"); break;
-      case SQLITE_FLOAT:   printf("Float  "); break;
-      case SQLITE_BLOB:    printf("Blob   "); break;
-      case SQLITE_NULL:    printf("Null   "); break;
-      case SQLITE_TEXT:    printf("Text   "); break;
-    }
-
-    printf(" -> ");
-
-    /* print column data */
-    printf("TODO");
-
-    printf("\n");
-  }
-
-  printf("End of Row\n-\n\n");
-}
-
 int execute_statement(sqlite3 *db, const char* zSql) {
-  sqlite3_stmt *stmt;
+  int nRow, nCol; /* the number of rows and columns in the result table */
+  char **zResult; /* the query results */
+  char *zErrorMsg = 0; /* the error message */
 
   /* print the statement */
   printf("> %s\n", zSql);
-  
-  /* prepare the statement */
-  if( sqlite3_prepare(db, zSql, -1, &stmt, 0) != SQLITE_OK ) {
-    return SQLITE_ERROR;
+
+  /* get the table */
+  int res = sqlite3_get_table(db, zSql, &zResult, &nRow, &nCol, &zErrorMsg);
+  if( res == SQLITE_OK ) {
+    /* print the table */
+    printf("Rows: %d, Cols: %d\n", nRow, nCol);
+    printf("ErrorMsg: %s\n", zErrorMsg);
+    printf("zResult: %p\n", zResult);
   }
 
-  /* execute the statement */
-  int done = 0;
-  int row_count = 0;
-  while( !done ) {
-    switch( sqlite3_step(stmt) ) {
-      case SQLITE_BUSY:
-        break;
-      case SQLITE_ROW:
-        print_statement_columns(stmt);
-        row_count++;
-        break;
-      case SQLITE_DONE:
-        done = 1;
-        break;
-      case SQLITE_ERROR:
-      case SQLITE_MISUSE:
-        return SQLITE_ERROR;
-    }
+  if( zResult ) {
+    sqlite3_free_table(zResult);
   }
 
-  printf("Printed %d rows\n\n", row_count);
-
-  /* free the statement from memory */
-  if( sqlite3_finalize(stmt) != SQLITE_OK ) {
-    return SQLITE_ERROR;
+  if( zErrorMsg ) {
+    sqlite3_free(zErrorMsg);
   }
 
-  return SQLITE_OK;
+  printf("\n");
+  return res;
 }
 
 int run_example_statements(sqlite3 *db) {
